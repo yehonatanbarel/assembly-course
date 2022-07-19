d9860698fcef50917c4e9c31d2495e870e10a392
yehonatan barel <yhbarel@gmail.com>
2022-07-19 17:59:20 +0300

    update

--- practiceFromLecture4.asm
+++ practiceFromLecture4.asm
@@ -6,11 +6,25 @@
 
 .data     #this will hold all the var we will use in our prog
 msg: .asciiz"\nPlease enter a number in the range -9999..9999\n"  #.asciiz
-
+error_msg: .asciiz"\nThe number is illegal. ENTER AGAIN\n"
 
 .text   #this tell the assembler that here the prog is beggin
 .global main    #    this and above are directive
 main:    #this is label
-li $v0,4    #in this way we print a string to the screen
-la $a0,msg
-syscall
+	li $v0,4    #in this way we print a string to the screen
+	la $a0,msg
+	syscall  # print welcome msg
+get_input:	
+	li $v0,5
+	syscall  #read int from keyboard
+	slti $t0,$v0,-9999
+	slti $t1,$v0,10000
+	not $t0,$t0
+	and $t2,$t1,$t0
+	bne $t2,$zero,continue # if number in range = continue
+	li $v0,4		# else try again
+	la $a0,error_msg
+	syscall
+	j get_input
+	
+continue:
